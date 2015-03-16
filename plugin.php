fd105fbc8e6cef3012b4658f2a8ad729c9beea9a
Gustavo Bordoni <bordoni.dev@gmail.com>
2015-03-16 20:50:55 -0300

    Improve tribe_is_photo

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       The Events Calendar: Snippet 948580
  * Plugin URI:        https://github.com/bordoni/tec-forum-support/tree/plugin-948580
  * Description:       The Events Calendar Support Addon
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Gustavo Bordoni
  * Author URI:        http://bordoni.me
  * License:           GPL-2.0+
@@ -34,8 +34,10 @@ class TEC_Forum_948580 {
 	}
 
 	public static function recurrence_tooltip( $tooltip ){
+		global $wp_query;
+
 		$post_id = get_the_ID();
-		if ( tribe_is_recurring_event( $post_id ) && tribe_is_photo() ) {
+		if ( tribe_is_recurring_event( $post_id ) && ( $wp_query->tribe_is_photo || tribe_is_photo() ) ) {
 			$tooltip = '';
 			$tooltip .= '<div class="recurringinfo">';
 			$tooltip .= '<div class="event-is-recurring">';
