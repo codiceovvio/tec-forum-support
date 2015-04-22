67f4381b6a6344ceef3b8f9b66f568d8ac26499a
Gustavo Bordoni <bordoni.dev@gmail.com>
2015-04-22 12:18:41 -0300

    Display Event details on RSS feed content

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name:       TEC Addon: Display order details on WooCommerce
- * Plugin URI:        https://github.com/bordoni/tec-forum-support/tree/plugin-955520
+ * Plugin Name:       TEC Addon: Display Event details on RSS feed content
+ * Plugin URI:        https://github.com/bordoni/tec-forum-support/tree/plugin-952243
  * Description:       The Events Calendar Support Addon
  * Version:           0.1.0
  * Author:            Gustavo Bordoni
@@ -14,17 +14,16 @@
 if ( ! defined( 'WPINC' ) ){
 	die;
 }
+// add_filter( "", "plugin_function_name" )
 
-class TEC_Forum_955520 {
+class TEC_Forum_952243 {
 
-	public static $ID = 955520;
+	public static $ID = 952243;
 
 	public static $_instance = null;
 
 	public function __construct(){
-		add_filter( 'woocommerce_cart_item_name', array( __CLASS__, 'cart_item_name' ), 10, 3 );
-		add_action( 'woocommerce_admin_order_item_values', array( __CLASS__, 'admin_order_item_values' ), 10, 3 );
-		add_action( 'woocommerce_admin_order_item_headers', array( __CLASS__, 'admin_order_item_headers' ) );
+		add_action( 'the_content_feed', array( __CLASS__, 'content_feed' ) );
 	}
 
 	public static function instance(){
@@ -35,35 +34,21 @@ class TEC_Forum_955520 {
 		return self::$_instance;
 	}
 
-	public static function admin_order_item_headers( ){
-		echo '<th class="tec-event sortable">' . esc_attr__( 'Event', 'woocommerce' ) . '</th>';
-	}
-
-	public static function admin_order_item_values( $_product, $item, $item_id ){
-		$ticket = $_product->id;
-		$event = get_post_meta( $ticket, '_tribe_wooticket_for_event', true );
-		if ( $event ) {
-			$value = sprintf( '<a href="%s" title="ID:%d" target="_blank"><strong>%s</strong></a>', get_edit_post_link( $event ), $event, get_the_title( $event ) );
+	public static function content_feed( $content ){
+		if ( ! tribe_is_event( get_the_ID() ) ){
+			return $content;
 		}
 
-		?>
-		<td class="tec-event" width="35%" data-sort-value="<?php echo absint( $event ); ?>">
-			<div class="view">
-				<?php echo wp_kses_post( $value ); ?>
-			</div>
-		</td>
-		<?php
+		$content .= '<hr>';
+		$content .= '<ul>';
+		$content .= '<li><b>' . esc_attr_e( 'Organizer', 'tribe-events-calendar' ) . ':</b> ' . tribe_get_organizer() . '</li>';
+		$content .= '<li><b>' . esc_attr_e( 'Venue', 'tribe-events-calendar' ) . ':</b> ' . tribe_get_venue() . '</li>';
+		$content .= '<li><b>' . esc_attr_e( 'Address', 'tribe-events-calendar' ) . ':</b> ' . tribe_get_full_address() . '</li>';
+		$content .= '<li><b>' . esc_attr_e( 'Google Map', 'tribe-events-calendar' ) . ':</b> ' . tribe_show_google_map_link() . '</li>';
+		$content .= '</ul>';
+
+		return $content;
 	}
 
-	public static function cart_item_name( $title, $values, $cart_item_key ){
-		$ticket_meta = get_post_meta( $values['product_id'] );
-		$event_id = absint( $ticket_meta['_tribe_wooticket_for_event'][0] );
-
-		if ( $event_id ) {
-			$title = sprintf( '%s for <a href="%s" target="_blank"><strong>%s</strong></a>', $title, get_permalink( $event_id ), get_the_title( $event_id ) );
-		}
-
-		return $title;
-	}
 }
-add_action( 'init', array( 'TEC_Forum_955520', 'instance' ) );
+add_action( 'init', array( 'TEC_Forum_952243', 'instance' ) );
