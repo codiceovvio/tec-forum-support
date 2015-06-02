96a28a11e929ffdfe17cca229eb4a69442dbcf87
Gustavo Bordoni <bordoni.dev@gmail.com>
2015-06-02 15:53:32 -0300

    Resolving the issue with the is_a

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       TEC Addon: Change Category labels
  * Plugin URI:        https://github.com/bordoni/tec-forum-support/tree/plugin-957928
  * Description:       The Events Calendar Support Addon
- * Version:           0.1.1
+ * Version:           0.1.2
  * Author:            Gustavo Bordoni
  * Author URI:        http://bordoni.me
  * License:           GPL-2.0+
@@ -41,7 +41,7 @@ class TEC_Forum_957928 {
 	}
 
 	public static function instance(){
-		if ( ! is_a( self::$_instance, __CLASS__ ) ) {
+		if ( ! ( self::$_instance instanceof self ) ) {
 			self::$_instance = new self();
 		}
 
