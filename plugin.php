b998616dc0e70f7d08320f38184f2aac49c7fb0a
Gustavo Bordoni <bordoni.dev@gmail.com>
2015-06-02 15:47:30 -0300

    Resolving the issue with the is_a

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       The Events Calendar: Snippet 948580
  * Plugin URI:        https://github.com/bordoni/tec-forum-support/tree/plugin-948580
  * Description:       The Events Calendar Support Addon
- * Version:           0.1.1
+ * Version:           0.1.2
  * Author:            Gustavo Bordoni
  * Author URI:        http://bordoni.me
  * License:           GPL-2.0+
@@ -26,7 +26,7 @@ class TEC_Forum_948580 {
 	}
 
 	public static function instance(){
-		if ( ! is_a( self::$_instance, __CLASS__ ) ) {
+		if ( ! ( self::$_instance instanceof self ) ) {
 			self::$_instance = new self();
 		}
 
