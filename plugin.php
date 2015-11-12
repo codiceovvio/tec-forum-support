f035a583990f88cb4d3afbc89137de28202cf3f5
bordoni <bordoni.dev@gmail.com>
2015-11-12 03:32:00 -0200

    Push QA plugin

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name:       TEC Addon: Eventbrite Timezone
- * Plugin URI:        https://github.com/bordoni/tec-forum-support/tree/plugin-968003
- * Description:       The Events Calendar Support Addon
+ * Plugin Name:       TEC Addon: Testing Filters CSV
+ * Plugin URI:        https://github.com/bordoni/tec-forum-support/tree/plugin-ticket-40224
+ * Description:       The Events Calendar QA Addon
  * Version:           0.1.0
  * Author:            Gustavo Bordoni
  * Author URI:        http://bordoni.me
@@ -15,103 +15,34 @@ if ( ! defined( 'WPINC' ) ){
 	die;
 }
 
-class TEC_Forum_968003 {
+class TEC_Ticket_40224 {
 
-	public static $ID = 968003;
+	public static $ID = 40224;
 
 	public static $_instance = null;
 
-	public function __construct(){
-		add_filter( 'http_request_args', array( __CLASS__, 'http_request_args' ), 15, 2 );
+	public function __construct() {
+		add_filter( 'tribe_events_tickets_attendees_csv_items', array( __CLASS__, 'filter_items' ), 15, 2 );
 	}
 
-	public static function instance(){
+	public static function instance() {
 		if ( ! ( self::$_instance instanceof self ) ) {
 			self::$_instance = new self();
 		}
-
 		return self::$_instance;
 	}
 
-	public static function http_request_args( $request, $url ) {
-		if ( ! class_exists( 'Tribe__Events__Tickets__Eventbrite__Main' ) ){
-			return $request;
-		}
-
-		if ( version_compare( Tribe__Events__Tickets__Eventbrite__Main::$pluginVersion, '3.11', '>=' ) ){
-			return $request;
-		}
-
-		$baseurl = apply_filters( 'tribe-eventbrite-base_api_url', 'https://www.eventbriteapi.com/v3/' );
-		if ( false === strpos( $url, $baseurl ) ){
-			return $request;
-		}
-
-		if (
-			false === strpos( $url, $baseurl . 'events' ) ||
-			false !== strpos( $url, '/unpublish/?token=' ) ||
-			false !== strpos( $url, '/unpublish?token=' ) ||
-			false !== strpos( $url, '/publish/?token=' ) ||
-			false !== strpos( $url, '/publish?token=' )
-		){
-			return $request;
-		}
-
-		if ( ! is_array( $request['body'] ) || ! isset( $request['body']['event.start.utc'], $request['body']['event.start.timezone'], $request['body']['event.end.utc'], $request['body']['event.end.timezone'] ) ){
-			return $request;
-		}
-
-		// Remove the Z param
-		$request['body']['event.start.utc'] = str_replace( 'Z', '', $request['body']['event.start.utc'] );
-		$request['body']['event.end.utc'] = str_replace( 'Z', '', $request['body']['event.end.utc'] );
-
-		// Add the timezone and the Z Param
-		$request['body']['event.start.utc'] = date( Tribe__Events__Tickets__Eventbrite__API::$date_format, self::wp_strtotime( $request['body']['event.start.utc'] ) );
-		$request['body']['event.end.utc'] = date( Tribe__Events__Tickets__Eventbrite__API::$date_format, self::wp_strtotime( $request['body']['event.end.utc'] ) );
-
-		return $request;
-	}
-
-	/**
-	 * Converts a locally-formatted date to a unix timestamp. This is a drop-in
-	 * replacement for `strtotime()`, except that where strtotime assumes GMT, this
-	 * assumes local time (as described below). If a timezone is specified, this
-	 * function defers to strtotime().
-	 *
-	 * If there is a timezone_string available, the date is assumed to be in that
-	 * timezone, otherwise it simply subtracts the value of the 'gmt_offset'
-	 * option.
-	 *
-	 * @see strtotime()
-	 * @uses get_option() to retrieve the value of 'gmt_offset'.
-	 * @param string $string A date/time string. See `strtotime` for valid formats.
-	 * @return int UNIX timestamp.
-	 */
-	private static function wp_strtotime( $string ) {
-		// If there's a timezone specified, we shouldn't convert it
-		try {
-			$test_date = new DateTime( $string );
-			if ( 'UTC' != $test_date->getTimezone()->getName() ) {
-				return strtotime( $string );
+	public static function filter_items( $items ) {
+		foreach ( $items as $key => $item ) {
+			if ( 0 === $key ) {
+				continue;
 			}
-		} catch ( Exception $e ) {
-			return strtotime( $string );
+
+			if ( 'Completed' === $item[1] ){
+				$item[7] = 'Checking Done!';
+			}
 		}
 
-		$tz = get_option( 'timezone_string' );
-		if ( ! empty( $tz ) ) {
-			$date = date_create( $string, new DateTimeZone( $tz ) );
-			if ( ! $date ) {
-				return strtotime( $string );
-			}
-			$date->setTimezone( new DateTimeZone( 'UTC' ) );
-			return $date->format( 'U' );
-		} else {
-			$offset = (float) get_option( 'gmt_offset' );
-			$seconds = intval( $offset * HOUR_IN_SECONDS );
-			$timestamp = strtotime( $string ) - $seconds;
-			return $timestamp;
-		}
 	}
 }
-add_action( 'plugins_loaded', array( 'TEC_Forum_968003', 'instance' ), 15 );
+add_action( 'plugins_loaded', array( 'TEC_Ticket_40224', 'instance' ), 15 );
